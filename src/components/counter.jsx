abe2e43a381e2e05c69fd390ae67afe021d9dab2
SHIVENDRA8004 <jat13632@gmail.com>
2022-09-06 02:00:20 +0530

    added internal styles to elements

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -4,12 +4,15 @@ class Counter extends Component {
         count: 0,
         imgUrl: "https://picsum.photos/200"
     };
+    style = {
+        fontSize: 20,
+        fontWeight: "bold"
+    }
     render() {
         return (
             <React.Fragment>
-                <span className='badge badge-primary m-2'>{this.countOption()}</span>
+                <span style={this.style} className='badge badge-primary m-2'>{this.countOption()}</span>
                 <button className='btn btn-secondary btn-sm'>Increament</button>
-                {/* <img src={this.state.imgUrl} alt="" /> */}
             </React.Fragment>
         );
     }
