d375cbed24cc183897357b9b94865c4f7aad892b
SHIVENDRA8004 <jat13632@gmail.com>
2022-09-07 22:51:52 +0530

    Rendered lists

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,13 +2,16 @@ import React, { Component } from 'react';
 class Counter extends Component {
     state = {
         count: 0,
-        imgUrl: "https://picsum.photos/200"
+        tags: ['tag1', 'tag2', 'tag3']
     };
     render() {
         return (
             <React.Fragment>
                 <span className={this.spanClass()}>{this.countOption()}</span>
                 <button className='btn btn-secondary btn-sm'>Increament</button>
+                <ul>
+                    {this.state.tags.map(tag => <li key={tag}>{tag}</li>)}
+                </ul>
             </React.Fragment>
         );
     }
