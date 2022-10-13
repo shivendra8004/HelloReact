d94b26d1bb693a3ece81553c1162faeaf7b3ba86
SHIVENDRA8004 <jat13632@gmail.com>
2022-10-13 07:44:00 +0530

    Tutorial 5 ended

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import './App.css';
-
+import Navbar from './components/Navbar';
 function App() {
-  const title = "Hello, Lets Learn React Variables and Dynamic Values";
-  const obj ={ name:"Shiva", age: 19, enr:""};
-  var link ;
-  var num=1;
-  if(num<4){
-    link="https://www.google.com";
-  }
-  else{
-    link="https://www.youtube.com";
-  }
+  const title = "Hello, Lets Learn Multiple Components";
   return (
     <div className="App">
+      <Navbar/>
       <div className="content">
-        <h1 className="heading">Dynamic Values</h1>
-        <strong>{title}</strong>
-        <p>{Math.random()*10}</p>
-        <p>{obj.name}</p>
-        <a href={link} target="_blank" rel="noreferrer" >Google</a>
+        <p>{title}</p>
       </div>
     </div>
   );
