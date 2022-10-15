5c00a241a1880f33118db287fc4b8163c51c9aa8
SHIVENDRA8004 <jat13632@gmail.com>
2022-10-15 23:39:37 +0530

    Tutorial 8 ended about state hooks

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,16 @@
+import { useState } from "react";
+
 const Home = () => {
-
-    const handleClick=(e)=>{
+    const [name,setName]=useState('Shiva');
+    const [age,setAge]=useState(25);
+    const handleClick=()=>{
+        setName("Shivendra");
+        setAge(19);
     }
-
     return ( 
         <div className="home">
             <h2>Home Page</h2>
+            <p >{name} is {age} years old</p>
             <button className="btn" onClick={handleClick}>
                 Click
             </button>
