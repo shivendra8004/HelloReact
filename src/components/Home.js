05a1d3d0c25dfbd5c81477eb42048815f3f85dca
SHIVENDRA8004 <jat13632@gmail.com>
2022-10-16 00:06:35 +0530

    Tut 9 ended about react dev tools

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,9 @@
 import { useState } from "react";
 
 const Home = () => {
-    const [name,setName]=useState('Shiva');
-    const [age,setAge]=useState(25);
-    const handleClick=()=>{
-        setName("Shivendra");
-        setAge(19);
-    }
     return ( 
         <div className="home">
             <h2>Home Page</h2>
-            <p >{name} is {age} years old</p>
-            <button className="btn" onClick={handleClick}>
-                Click
-            </button>
         </div>
      );
 }
