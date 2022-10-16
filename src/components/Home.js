8d09366ae2100a21c7e2f708cb1bb9d18c3b251a
SHIVENDRA8004 <jat13632@gmail.com>
2022-10-16 10:19:59 +0530

    Tut 10 ended

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,8 @@ const Home = () => {
     const [blogs,setBlogs]=useState([
         {title:'My new Website',body:'Lorem lepsum....',author:'mario',id:1},
         {title:'Welcome Party',body:'Lorem lepsum....',author:'Yoshi',id:2},
-        {title:'Web dev top tips',body:'Lorem lepsum....',author:'Shiva',id:3}
+        {title:'Web dev top tips',body:'Lorem lepsum....',author:'Shiva',id:3},
+        {title:'Our God',body:'Jai shree ram....',author:'Hello',id:4}
     ]);
     return ( 
         <div className="home"> 
