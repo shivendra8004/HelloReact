a719fb519edeb5cab9a11248397208b16449d4a6
SHIVENDRA8004 <jat13632@gmail.com>
2022-10-16 09:31:27 +0530

    Tut 10 start

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,14 @@
 import { useState } from "react";
 
 const Home = () => {
+    const [blogs,setBlogs]=useState([
+        {title:'My new Website',body:'Lorem lepsum....',author:'mario',id:1},
+        {title:'Welcome Party',body:'Lorem lepsum....',author:'Yoshi',id:2},
+        {title:'Web dev top tips',body:'Lorem lepsum....',author:'Shiva',id:3}
+    ]);
     return ( 
-        <div className="home">
-            <h2>Home Page</h2>
+        <div className="home"> 
+        <p>{}</p>
         </div>
      );
 }
