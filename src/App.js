eddcc17a2928796f63329ee9b0787ff3435ca31b
SHIVENDRA8004 <jat13632@gmail.com>
2023-01-11 02:50:23 +0530

    Learned using this.state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
 import React, { Component } from "react";
 import "./App.css";
-// import Greet from "./components/Greet";
-import Welcome from "./components/Welcome";
-// import Hello from "./components/Hello";
+import Message from "./components/Message";
 class App extends Component {
   render() {
     return (
       <div className="App">
-        {/* <Greet name="Shiva" age="19">
-          <p>He is a Sweet Boy</p>
-        </Greet>
-        <Greet name="Hello" age="21">
-          <button>Click Me</button>
-        </Greet> */}
-        <Welcome name="Shiv" age="19">
-          <p>Full Name is Shivendra</p>
-        </Welcome>
-        <Welcome name="Shivendra" age="21">
-          <p>Nick Name is Shiva</p>
-        </Welcome>
-        {/* <Hello /> */}
+        <Message />
       </div>
     );
   }
