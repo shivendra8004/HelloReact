59b8a953b334706eca28d25def58c5d46592681e
SHIVENDRA8004 <jat13632@gmail.com>
2023-01-11 01:57:41 +0530

    Learned using propes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React, { Component } from "react";
 import "./App.css";
-// import Greet from "./components/Greet";
+import Greet from "./components/Greet";
 // import Welcome from "./components/Welcome";
-import Hello from "./components/Hello";
+// import Hello from "./components/Hello";
 class App extends Component {
   render() {
     return (
       <div className="App">
-        {/* <Greet />
-        <Welcome /> */}
-        <Hello />
+        <Greet name="Shiva" />
+        <Greet name="Hello" />
+        {/* <Welcome /> */}
+        {/* <Hello /> */}
       </div>
     );
   }
--- src/components/Greet.js
+++ src/components/Greet.js
@@ -4,7 +4,7 @@ import React from "react";
 //   return <h1>Hello Shiva</h1>;
 // }
 
-const Greet = () => {
-  return <h1>Hello Shiva</h1>;
+const Greet = (props) => {
+  return <h1>Hello {props.name}</h1>;
 };
 export default Greet;
