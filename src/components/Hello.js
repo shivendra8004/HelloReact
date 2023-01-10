0f273a4a60b6a2ff530777276e4d6f9b3186f576
SHIVENDRA8004 <jat13632@gmail.com>
2023-01-11 01:42:57 +0530

    Created React element

--- src/components/Hello.js
+++ src/components/Hello.js
@@ -1,10 +1,11 @@
 import React from "react";
 
 const Hello = () => {
-  return (
-    <div>
-      <h1>Hello Shiv</h1>
-    </div>
-  );
+  //   return (
+  //     <div>
+  //       <h1>Hello Shiv</h1>
+  //     </div>
+  //   );
+  return React.createElement("div", null, "hello Shiv");
 };
 export default Hello;
