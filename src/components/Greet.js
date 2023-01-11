1687c17ea8ef19e6d1c19ca631c1385a9022a1d4
SHIVENDRA8004 <jat13632@gmail.com>
2023-01-11 18:02:41 +0530

    Class component

--- src/components/Greet.js
+++ src/components/Greet.js
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { Component } from "react";
 
-const Greet = () => {
-  return (
-    <div>
-      <h1>Functional Component</h1>
-    </div>
-  );
-};
+class Greet extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Class Component</h1>
+      </div>
+    );
+  }
+}
 export default Greet;
