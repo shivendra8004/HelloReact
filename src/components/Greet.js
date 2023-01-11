fad7ad04f34cdfb82e5f8d19b4f8dc53c2fd5afc
SHIVENDRA8004 <jat13632@gmail.com>
2023-01-11 18:22:26 +0530

    Learned using state

--- src/components/Greet.js
+++ src/components/Greet.js
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { Component } from "react";
 
-const Greet = (props) => {
-  return (
-    <div>
-      <h1>Hello {props.name} Using Functional Component</h1>
-    </div>
-  );
-};
+class Greet extends Component {
+  constructor() {
+    super();
+    this.state = {
+      message: "Hello ",
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>
+          {this.state.message} {this.props.name}
+        </h1>
+        <button
+          onClick={() => {
+            this.changeMessage();
+          }}
+        >
+          Subscribe
+        </button>
+      </div>
+    );
+  }
+}
 
 export default Greet;
