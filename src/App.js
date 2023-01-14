d03dfbbe4f2c8a9d01e96807a31c427e26dcbf9e
SHIVENDRA8004 <jat13632@gmail.com>
2023-01-14 19:47:50 +0530

    Vid 11 SetState

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
 import "./App.css";
-import Greet from "./components/Greet";
+import Counter from "./components/Counter";
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Greet name="Shivendra" />
+        <Counter />
       </div>
     );
   }
---
+++ src/components/Counter.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  render() {
+    return <div>Counter</div>;
+  }
+}
+
+export default Counter;
