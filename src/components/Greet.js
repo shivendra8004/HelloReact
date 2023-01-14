0c3fae115574b722b1df7c378c157d9ef65879ed
SHIVENDRA8004 <jat13632@gmail.com>
2023-01-15 00:15:47 +0530

    Destructring props in class component

--- src/components/Greet.js
+++ src/components/Greet.js
@@ -1,11 +1,18 @@
 import React, { Component } from "react";
 
 export class Greet extends Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 4,
+    };
+  }
   render() {
     const { name, age } = this.props;
+    const { count } = this.state;
     return (
       <div>
-        {name} {age}
+        {name} {age} {count}
       </div>
     );
   }
