25801f4406ab0d185e2c8c430c6f51a5d67dfaa5
SHIVENDRA8004 <jat13632@gmail.com>
2023-01-14 20:00:07 +0530

    Vid 11 SetState

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,8 +1,43 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 0,
+    };
+  }
+  increament() {
+    this.setState({
+      count: this.state.count + 1,
+    });
+  }
+  decreament() {
+    this.setState({
+      count: this.state.count - 1,
+    });
+  }
   render() {
-    return <div>Counter</div>;
+    return (
+      <div>
+        <h1>Counter</h1>
+        <p>{this.state.count}</p>
+        <button
+          onClick={() => {
+            this.increament();
+          }}
+        >
+          Increament
+        </button>
+        <button
+          onClick={() => {
+            this.decreament();
+          }}
+        >
+          Decreament
+        </button>
+      </div>
+    );
   }
 }
 
