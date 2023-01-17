3cd76732f1b4e489243f689031b82cbe2d67bcb9
SHIVENDRA8004 <jat13632@gmail.com>
2023-01-18 00:09:36 +0530

    Lect 7

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,18 @@
 const Home = () => {
+  const handleClick = () => {
+    console.log("Hello Guys");
+  };
   return (
     <div className="home">
       <h2>Home Page</h2>
+      <button
+        onClick={() => {
+          handleClick();
+        }}
+      >
+        Click Here
+      </button>
     </div>
   );
 };
-
 export default Home;
