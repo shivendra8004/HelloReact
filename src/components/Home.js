9bf18a9e491afd558b42d31327d656ebea4ada3b
SHIVENDRA8004 <jat13632@gmail.com>
2023-01-18 00:40:54 +0530

    Lect 8 useState hook

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,13 +3,17 @@ import { useState } from "react";
 
 const Home = () => {
   let [name, setName] = useState("Shivendra Jat");
+  const [age, setAge] = useState(19);
   const handleClick = () => {
     setName("Shiva");
+    setAge(20);
   };
   return (
     <div className="home">
       <h2>Home Page</h2>
-      <p> {name} </p>
+      <p>
+        {name} {age}
+      </p>
       <button onClick={() => handleClick()}>Click Here</button>
     </div>
   );
