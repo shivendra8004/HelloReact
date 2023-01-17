188f3f36c2d216c58fe763e96f75ab5c1c93373d
SHIVENDRA8004 <jat13632@gmail.com>
2023-01-18 00:25:35 +0530

    Lect 8 useState hook

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,13 @@
 const Home = () => {
+  let name = "Shivendra Jat";
   const handleClick = () => {
     console.log("Hello Guys");
+    name = "Shiva";
   };
   return (
     <div className="home">
       <h2>Home Page</h2>
+      <p> {name} </p>
       <button onClick={() => handleClick()}>Click Here</button>
     </div>
   );
