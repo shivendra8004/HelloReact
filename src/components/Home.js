9eec2f7a6a6454cbecdb9cd1ff75969bdce7e30c
SHIVENDRA8004 <jat13632@gmail.com>
2023-01-18 00:47:53 +0530

    Lect 10 outputting lists

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,20 +1,8 @@
-import React from "react";
-import { useState } from "react";
-
+import React, { useState } from "react";
 const Home = () => {
-  let [name, setName] = useState("Shivendra Jat");
-  const [age, setAge] = useState(19);
-  const handleClick = () => {
-    setName("Shiva");
-    setAge(20);
-  };
   return (
     <div className="home">
       <h2>Home Page</h2>
-      <p>
-        {name} {age}
-      </p>
-      <button onClick={() => handleClick()}>Click Here</button>
     </div>
   );
 };
