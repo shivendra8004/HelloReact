41547b92f290edbe435a07990b24c6bafb00c965
SHIVENDRA8004 <jat13632@gmail.com>
2023-01-17 07:04:13 +0530

    Done

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,23 +1,33 @@
 import { useState } from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
-    const [blogs,setBlogs]=useState([
-        {title:'My new Website',body:'Lorem lepsum....',author:'mario',id:1},
-        {title:'Welcome Party',body:'Lorem lepsum....',author:'Yoshi',id:2},
-        {title:'Web dev top tips',body:'Lorem lepsum....',author:'Shiva',id:3},
-        {title:'Our God',body:'Jai shree ram....',author:'Hello',id:4}
-    ]);
-    return ( 
-        <div className="home"> 
-        {blogs.map((blog)=>(
-            <div className="blog-preview" key={blog.id}>
-                <h2>{blog.title}</h2>
-                <p>Written by {blog.author}</p>
-                <p>{blog.body}</p>
-            </div>
-        ))}
-        </div>
-     );
-}
- 
+  const [blogs, setBlogs] = useState([
+    {
+      title: "My new Website",
+      body: "Lorem lepsum....",
+      author: "mario",
+      id: 1,
+    },
+    {
+      title: "Welcome Party",
+      body: "Lorem lepsum....",
+      author: "Yoshi",
+      id: 2,
+    },
+    {
+      title: "Web dev top tips",
+      body: "Lorem lepsum....",
+      author: "Shiva",
+      id: 3,
+    },
+    { title: "Our God", body: "Jai shree ram....", author: "Hello", id: 4 },
+  ]);
+  return (
+    <div className="home">
+      <BlogList blogs={blogs} title="All Blogs" />
+    </div>
+  );
+};
+
 export default Home;
