5744e65ffb66a8caa9c29919eaa7c70f1b0dcf3b
SHIVENDRA8004 <jat13632@gmail.com>
2023-01-17 08:02:28 +0530

    Lect 5 Multiple components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
 function App() {
-  const title = "Hey Welcome to the New blog";
-  const likes = 50;
-  const link =
-    "https://www.youtube.com/watch?v=0sSYmRImgRY&list=PL4cUxeGkcC9gZD-Tvwfod2gaISzfRiP9d&index=5";
   return (
     <div className="App">
+      <Navbar />
       <div className="content">
-        <h1>Dojo Blog</h1>
-        <p>{title}</p>
-        <h4>Likes {likes} times</h4>
-        <a href={link}> Youtube</a>
+        <Home />
       </div>
     </div>
   );
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,31 +1,7 @@
-import { useState } from "react";
-import BlogList from "./BlogList";
-
 const Home = () => {
-  const [blogs] = useState([
-    {
-      title: "My new Website",
-      body: "Lorem lepsum....",
-      author: "mario",
-      id: 1,
-    },
-    {
-      title: "Welcome Party",
-      body: "Lorem lepsum....",
-      author: "Yoshi",
-      id: 2,
-    },
-    {
-      title: "Web dev top tips",
-      body: "Lorem lepsum....",
-      author: "Shiva",
-      id: 3,
-    },
-    { title: "Our God", body: "Jai shree ram....", author: "Hello", id: 4 },
-  ]);
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All Blogs" />
+      <h2>Home Page</h2>
     </div>
   );
 };
