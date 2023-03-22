18ce73c41800f064cd4385a2cbc9932a4f7c4c74
SHIVENDRA8004 <jat13632@gmail.com>
2023-03-23 02:12:48 +0530

    Learned all about props

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -1,7 +1,9 @@
 const BlogList = (props) => {
   const blogs = props.blogs;
+  const heading = props.title;
   return (
     <div className="blog-list">
+      <h2>{heading}</h2>
       {blogs.map((blog) => (
         <div className="blog-preview" key={blog.id}>
           <h2>{blog.title}</h2>
