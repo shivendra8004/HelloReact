1d3f183575cfffc2276fa016c08e492841273a6b
SHIVENDRA8004 <jat13632@gmail.com>
2023-03-25 19:32:16 +0530

    Removed blogs from home page folder

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,44 +1,7 @@
 import React, { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 const Home = () => {
-  let [blogs, setBlogs] = useState([
-    {
-      title: "My New Website",
-      body: " wqeuher qwotjdaf asfhp wet wqea hgdfdsk ew glaff hgel  ",
-      author: "Shivendra Jat",
-      id: 1,
-    },
-    {
-      title: "Welcome Party ",
-      body: " wqeuher qwotjdaf asfhp wet wqea hgdfdsk ew glaff hgel  ",
-      author: "Jyoti Kumari",
-      id: 2,
-    },
-    {
-      title: "Web Dev Tips and Tricks",
-      body: " wqeuher qwotjdaf asfhp wet wqea hgdfdsk ew glaff hgel  ",
-      author: "Anindya Ram",
-      id: 3,
-    },
-    {
-      title: "The Net Ninja Channel",
-      body: " wqeuher qwotjdaf asfhp wet wqea hgdfdsk ew glaff hgel  ",
-      author: "The Net Ninja",
-      id: 4,
-    },
-    {
-      title: "Web Dev Course",
-      body: " wqeuher qwotjdaf asfhp wet wqea hgdfdsk ew glaff hgel  ",
-      author: "Shivendra Jat",
-      id: 5,
-    },
-    {
-      title: "FSD Bootcamp",
-      body: " wqeuher qwotjdaf asfhp wet wqea hgdfdsk ew glaff hgel  ",
-      author: "Shivendra Jat",
-      id: 6,
-    },
-  ]);
+  let [blogs, setBlogs] = useState(null);
   const handleDelete = (id) => {
     const newBlogs = blogs.filter((blog) => blog.id !== id);
     setBlogs(newBlogs);
