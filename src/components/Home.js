bb85c59ae752f841f9f3593522faf40f82f310ca
SHIVENDRA8004 <jat13632@gmail.com>
2023-03-27 22:01:50 +0530

    Removed all errors by removing setError from if condn

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,11 +6,10 @@ const Home = () => {
   const [error, setError] = useState(null);
   useEffect(() => {
     setLoading(true);
-    fetch("http://localhost:5500/blogs")
+    fetch("http://localhost:5500/blogsd")
       .then((res) => {
         console.log(res);
         if (!res.ok) {
-          setError("Could not fetch data from database");
           throw Error("Could not fetch data from database");
         }
         return res.json();
@@ -20,12 +19,12 @@ const Home = () => {
         setLoading(false);
       })
       .catch((error) => {
-        setError(error);
+        setError(error.message);
       });
   }, []);
   return (
     <div className="home">
-      {error && <div>There is any Error...</div>}
+      {error && <div className="errorDiv "> {error}</div>}
 
       {loading && <div>Loading Blogs...</div>}
       {blogs && !error && <BlogList blogs={blogs} heading={"All Blogs"} />}
