bb90a5e36adc895478280de4c3dca5227446b947
SHIVENDRA8004 <jat13632@gmail.com>
2023-03-28 20:32:45 +0530

    Successfully created a custom hook for fetching data from server and returing it to browser

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,12 @@
-import React, { useState, useEffect } from "react";
 import BlogList from "./BlogList";
+import useFetch from "../useFetch";
+
 const Home = () => {
+  const { data, loading, error } = useFetch("http://localhost:5500/blogs");
+  const blogs = data;
   return (
     <div className="home">
       {error && <div className="errorDiv "> {error}</div>}
-
       {loading && <div className="loadingDiv">Loading Blogs...</div>}
       {blogs && !error && <BlogList blogs={blogs} heading={"All Blogs"} />}
     </div>
--- src/useFetch.js
+++ src/useFetch.js
@@ -3,7 +3,7 @@ const useFetch = (url) => {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
-  useEffect(() => {
+  useEffect((url) => {
     setLoading(true);
     fetch(url)
       .then((res) => {
