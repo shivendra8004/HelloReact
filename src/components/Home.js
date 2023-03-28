10dcd1c02970f20d3c6a56ab72e5dd2526b7a3df
SHIVENDRA8004 <jat13632@gmail.com>
2023-03-28 20:23:45 +0530

    Creating my very first custom hook for fetching data from json-server

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,28 +1,6 @@
 import React, { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 const Home = () => {
-  const [blogs, setBlogs] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
-  useEffect(() => {
-    setLoading(true);
-    fetch("http://localhost:5500/blogs")
-      .then((res) => {
-        console.log(res);
-        if (!res.ok) {
-          throw Error("Could not fetch data from database");
-        }
-        return res.json();
-      })
-      .then((data) => {
-        setBlogs(data);
-        setLoading(false);
-        setError(null);
-      })
-      .catch((error) => {
-        setError(error.message);
-      });
-  }, []);
   return (
     <div className="home">
       {error && <div className="errorDiv "> {error}</div>}
---
+++ src/useFetch.js
@@ -0,0 +1,24 @@
+const useFetch = () => {
+  const [blogs, setBlogs] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  useEffect(() => {
+    setLoading(true);
+    fetch("http://localhost:5500/blogs")
+      .then((res) => {
+        console.log(res);
+        if (!res.ok) {
+          throw Error("Could not fetch data from database");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setBlogs(data);
+        setLoading(false);
+        setError(null);
+      })
+      .catch((error) => {
+        setError(error.message);
+      });
+  }, []);
+};
