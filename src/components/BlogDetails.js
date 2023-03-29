0c233aa774fdb775ceeaf0ba4608fcbf9ee21adb
SHIVENDRA8004 <jat13632@gmail.com>
2023-03-29 19:00:51 +0530

    Error fixed, error due to starting component name starting with lower letter

--- src/components/BlogDetails.js
+++ src/components/BlogDetails.js
@@ -1,15 +1,15 @@
 import { useParams } from "react-router-dom";
 
-const blogDetails = () => {
-  const { id } = useParams;
+const BlogDetails = () => {
+  const { id } = useParams();
   return (
     <>
       <div>
         <h2>Blog Details</h2>
-        <p>Blog Number {id}</p>
+        <p>Blog Number {id} </p>
       </div>
     </>
   );
 };
 
-export default blogDetails;
+export default BlogDetails;
