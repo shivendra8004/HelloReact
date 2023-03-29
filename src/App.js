b5e2b14a27d2b34da87807062d18ee1f457e942f
SHIVENDRA8004 <jat13632@gmail.com>
2023-03-29 16:49:06 +0530

    Added exact match routes to the route of each component and now its working

--- src/App.js
+++ src/App.js
@@ -9,10 +9,10 @@ function App() {
         <Navbar />
         <div className="content">
           <Switch>
-            <Route path="/">
+            <Route exact path="/">
               <Home />
             </Route>
-            <Route path="/create">
+            <Route exact path="/create">
               <Create />
             </Route>
           </Switch>
