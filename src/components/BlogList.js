96cfe22f41e15a94d6f2214946a0d387ac5bc96c
SHIVENDRA8004 <jat13632@gmail.com>
2023-03-29 19:04:58 +0530

    Added link component on Blog list div to go to details page of that blog

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -1,12 +1,16 @@
+import { Link } from "react-router-dom";
+
 const BlogList = ({ blogs, heading }) => {
   return (
     <div className="blog-list">
       <h2>{heading}</h2>
       {blogs.map((blog) => (
-        <div className="blog-preview" key={blog.id}>
-          <h2>{blog.title}</h2>
-          <p>Written by {blog.author}</p>
-        </div>
+        <Link to={`/blogs/${blog.id}`}>
+          <div className="blog-preview" key={blog.id}>
+            <h2>{blog.title}</h2>
+            <p>Written by {blog.author}</p>
+          </div>
+        </Link>
       ))}
     </div>
   );
