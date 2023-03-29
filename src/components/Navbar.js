a4d75ccd1634df110dc0652fe22edf835457b298
SHIVENDRA8004 <jat13632@gmail.com>
2023-03-29 16:54:47 +0530

    Using Link comaponent in navbar to stop sending request to server for loading different page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,11 @@
+import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <nav className="navbar">
       <h1>The Blossom Blogs</h1>
       <div className="links">
-        <a href="/">Home</a>
-        <a href="/create" className="newBlog">
-          New Blog
-        </a>
+        <Link to="/">Home</Link>
+        <Link to="/create">New Blog</Link>
       </div>
     </nav>
   );
