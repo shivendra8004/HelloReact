403443fadb864736b5c2bfa6b2debd89f94eada2
SHIVENDRA8004 <jat13632@gmail.com>
2023-04-01 18:33:28 +0530

    Added functionality of showing deleting

--- src/components/BlogDetails.js
+++ src/components/BlogDetails.js
@@ -1,17 +1,22 @@
 import { useParams } from "react-router-dom";
 import useFetch from "../useFetch";
+import { useState } from "react";
 const BlogDetails = () => {
+  const [deleting, setDeleting] = useState(false);
   const { id } = useParams();
   const { data: blog, error, loading } = useFetch("http://localhost:5500/blogs/" + id);
   const HandleDelete = (id) => {
+    setDeleting(true);
     fetch("http://localhost:5500/blogs/" + blog.id, {
       method: "DELETE",
     })
       .then(() => {
         console.log("Blog deleted");
+        setDeleting(false);
       })
       .catch((error) => {
         console.log(error);
+        setDeleting(false);
       });
   };
   return (
@@ -23,7 +28,8 @@ const BlogDetails = () => {
           <h2>{blog.title}</h2>
           <h3>Written by {blog.author}</h3>
           <p>{blog.body}</p>
-          <button onClick={HandleDelete}>Delete</button>
+          {!deleting && <button onClick={HandleDelete}>Delete</button>}
+          {deleting && <button disabled>Deleting..</button>}
         </article>
       )}
     </div>
