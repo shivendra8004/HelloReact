a3d2693c4528dcc8212b1da5140a0507035989a8
SHIVENDRA8004 <jat13632@gmail.com>
2023-04-01 18:43:30 +0530

    Added route for 404 page in app.js

--- src/components/404.js
+++ src/components/404.js
@@ -4,7 +4,7 @@ const Error = () => {
     <div className="not-found">
       <h2>Oops, Sorry</h2>
       <p>That page does'nt Exists</p>
-      <Link to="/">Go To Home</Link>
+      <Link to="/">Come Back Home</Link>
     </div>
   );
 };
