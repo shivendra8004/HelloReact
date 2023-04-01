5b002af93f1e6d16b95f8d9039600cf2728d2c23
SHIVENDRA8004 <jat13632@gmail.com>
2023-04-01 18:27:32 +0530

    Added functionality to remove blog

--- src/components/BlogDetails.js
+++ src/components/BlogDetails.js
@@ -3,6 +3,13 @@ import useFetch from "../useFetch";
 const BlogDetails = () => {
   const { id } = useParams();
   const { data: blog, error, loading } = useFetch("http://localhost:5500/blogs/" + id);
+  const HandleDelete = (id) => {
+    fetch("http://localhost:5500/blogs/" + id, {
+      method: "DELETE",
+    }).then(() => {
+      console.log("Blog deleted");
+    });
+  };
   return (
     <div className="blog-details">
       {loading && <div>Loading...</div>}
@@ -12,7 +19,7 @@ const BlogDetails = () => {
           <h2>{blog.title}</h2>
           <h3>Written by {blog.author}</h3>
           <p>{blog.body}</p>
-          <button>Delete</button>
+          <button onClick={HandleDelete}>Delete</button>
         </article>
       )}
     </div>
