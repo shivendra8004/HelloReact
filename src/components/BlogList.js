eabdf66b12489dd24ce872e687012a75359e694e
SHIVENDRA8004 <jat13632@gmail.com>
2023-04-02 23:43:00 +0530

    All error fixed

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -4,19 +4,21 @@ const BlogList = ({ blogs, heading }) => {
   return (
     <div className="blog-list">
       <h2>{heading}</h2>
-      {(blogs.length)? (blogs.map((blog) => (
-        <Link to={`/blogs/${blog.id}`}>
-          <div className="blog-preview" key={blog.id}>
-            <h2>{blog.title}</h2>
-            <p>Written by {blog.author}</p>
-          </div>
-        </Link>
-      )):(
+      {blogs.length ? (
+        blogs.map((blog) => (
+          <Link to={`/blogs/${blog.id}`}>
+            <div className="blog-preview" key={blog.id}>
+              <h2>{blog.title}</h2>
+              <p>Written by {blog.author}</p>
+            </div>
+          </Link>
+        ))
+      ) : (
         <div>
           <h2>No Posts</h2>
           <p>The posts you create will show up here!</p>
         </div>
-      ))}
+      )}
     </div>
   );
 };
